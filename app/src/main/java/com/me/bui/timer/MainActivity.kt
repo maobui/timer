18d59bec0bfd6de550eaf023b8905d39009e5499
maobui <mao.buidinh@hotmail.com>
2019-09-30 15:32:31 +0700

    - Count down with rx.

--- app/src/main/java/com/me/bui/timer/MainActivity.kt
+++ app/src/main/java/com/me/bui/timer/MainActivity.kt
@@ -3,22 +3,39 @@ package com.me.bui.timer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.Transformations.map
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import io.reactivex.functions.Predicate
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private var countDownTimer: CountDownTimer? = null
-    val TIME_INTERVAL = 1000L
-    var timeCount = 60000L
-    var status: Status = Status.STOP
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+    private val TIME_UNIT = 1000L
+    private var timeCount = 60
     enum class Status(val value: Int) {
         START(0),
-        STOP(1)
+        PAUSE(1),
+        STOP(2)
     }
+
+    private val tick: BehaviorSubject<Long> = BehaviorSubject.createDefault(0L)
+    private val status: BehaviorSubject<Status> = BehaviorSubject.createDefault(Status.STOP)
+    private var timePause  = 0
+    private var disposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +44,50 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         imgReset.setOnClickListener(this)
         imgStartStop.setOnClickListener(this)
         imgReset.visibility = View.INVISIBLE
+
+        compositeDisposable.add(tick.toFlowable(BackpressureStrategy.BUFFER)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    Log.d("AAA", "tick--------------$it")
+                    progressCircular.progress = it.toInt()
+                    tvTime.text = hmsTimeFormatter(it * TIME_UNIT)
+
+                },
+                { it.printStackTrace() }
+            )
+        )
+        compositeDisposable.add(status.toFlowable(BackpressureStrategy.BUFFER)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    imgStartStop.setImageDrawable(resources.getDrawable(if (it != Status.STOP) R.drawable.ic_stop else R.drawable.ic_start, this.theme))
+                    when(it) {
+                        Status.STOP -> {
+                            setProgressBarValues()
+                        }
+                        Status.START -> startTimer(if(timePause > 0) timePause else timeCount)
+                        Status.PAUSE -> {
+                            disposable?.dispose()
+                            edtMinute.isEnabled = false
+                            imgReset.visibility = View.INVISIBLE
+                            Log.d("AAA", "pause--------------$timeCount  pause $timePause")
+                        }
+                    }
+                }
+                , { it.printStackTrace() }
+            )
+        )
+    }
+
+
+
+    override fun onDestroy() {
+        compositeDisposable.dispose()
+        disposable?.takeIf { !it.isDisposed }?.dispose()
+        super.onDestroy()
     }
 
     override fun onClick(p0: View?) {
@@ -37,58 +98,64 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun onReset() {
-        stopTimer()
-        startTimer()
+        status.onNext(Status.START)
     }
 
     private fun onStartStop() {
-        imgStartStop.setImageDrawable(resources.getDrawable(if (status == Status.STOP) R.drawable.ic_stop else R.drawable.ic_start, this.theme))
-        if(status == Status.STOP) {
-            setTimer()
-            setProgressBarValues()
-            startTimer()
-        } else {
-            stopTimer()
-        }
-    }
-
-    private fun startTimer() {
-        imgReset.visibility = View.INVISIBLE
-        countDownTimer = object : CountDownTimer(timeCount, TIME_INTERVAL) {
-            override fun onFinish() {
-                status = Status.STOP
+        when (status.value) {
+            Status.START -> status.onNext(Status.PAUSE)
+            Status.PAUSE -> status.onNext(Status.START)
+            Status.STOP -> {
+                setTimer()
                 setProgressBarValues()
-                imgReset.visibility = View.VISIBLE
+                status.onNext(Status.START)
             }
-
-            override fun onTick(tick: Long) {
-                tvTime.text = hmsTimeFormatter(tick)
-                progressCircular.progress = (tick / 1000).toInt()
-            }
-
         }
-        status = Status.START
-        countDownTimer?.start()
     }
 
-    private fun stopTimer() {
-        status = Status.STOP
-        countDownTimer?.cancel()
+    fun startTimer( timeInSecond: Int) {
+        disposable =  Flowable.interval(0, 1, TimeUnit.SECONDS)
+            .take((timeInSecond + 1).toLong())
+            .doOnNext { Log.d("AAA", "doOnNext--------------$timeInSecond  it $it") }
+            .map { it -> timeInSecond - it - 1
+            }
+            .filter{ aLong -> aLong >= 0 && status.value == Status.START }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                edtMinute.isEnabled = false
+                imgReset.visibility = View.INVISIBLE
+                imgStartStop.setImageDrawable(resources.getDrawable(R.drawable.ic_stop, this.theme))
+            }
+            .doFinally {
+                edtMinute.isEnabled = true
+                imgReset.visibility = View.VISIBLE
+                imgStartStop.setImageDrawable(resources.getDrawable(R.drawable.ic_start, this.theme))
+                if (timePause == 0) status.onNext(Status.STOP)
+            }
+            .subscribe(
+                {
+                    tick.onNext(it)
+                    timePause = it.toInt()
+                    Log.d("AAA", "--------------$it  pause $timePause value ${status.value}")
+                },
+                { it.printStackTrace() }
+
+            )
     }
 
-    private fun  setTimer() {
-        if(edtMinute.text.isNullOrBlank()) {
+    private fun setTimer() {
+        if (edtMinute.text.isNullOrBlank()) {
             Toast.makeText(this, "Timer must not null", Toast.LENGTH_LONG).show()
         } else {
-            timeCount = edtMinute.text.toString().toLong() * TIME_INTERVAL
+            timeCount = edtMinute.text.toString().toInt()
         }
     }
 
     private fun setProgressBarValues() {
-        progressCircular.max = (timeCount / 1000).toInt()
-        progressCircular.progress = (timeCount / 1000).toInt()
+        progressCircular.max = timeCount
+        progressCircular.progress = timeCount
     }
-
 
 
     private fun hmsTimeFormatter(milliSeconds: Long): String {
@@ -96,9 +163,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             "%02d:%02d:%02d",
             TimeUnit.MILLISECONDS.toHours(milliSeconds),
             TimeUnit.MILLISECONDS.toMinutes(milliSeconds) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milliSeconds)),
-            TimeUnit.MILLISECONDS.toSeconds(milliSeconds) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliSeconds)
-            )
-        )
+            TimeUnit.MILLISECONDS.toSeconds(milliSeconds) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliSeconds)))
     }
 
 }
